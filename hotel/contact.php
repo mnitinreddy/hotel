b321b4adac7091bd59d6eba36a4bb2c17a2966a0
mnitinreddy <42674009+mnitinreddy@users.noreply.github.com>
2022-11-26 22:20:17 -0600

    Add files via upload

---
+++ hotel/contact.php
@@ -0,0 +1,116 @@
+<html>
+<head>
+  
+  <link href=" assets/img/favicon.png  " rel="icon">
+  <link href=" assets/img/apple-touch-icon.png  " rel="apple-touch-icon">
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i" rel="stylesheet">
+
+  <link href=" assets/vendor/animate.css/animate.min.css  " rel="stylesheet">
+  <link href=" assets/vendor/bootstrap/css/bootstrap.min.css  " rel="stylesheet">
+  <link href=" assets/vendor/bootstrap-icons/bootstrap-icons.css  " rel="stylesheet">
+  <link href=" assets/vendor/boxicons/css/boxicons.min.css  " rel="stylesheet">
+  <link href=" assets/vendor/glightbox/css/glightbox.min.css  " rel="stylesheet">
+  <link href=" assets/vendor/swiper/swiper-bundle.min.css  " rel="stylesheet">
+  <link href=" assets/css/style.css  " rel="stylesheet">
+</head>
+
+<body>
+    <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+    
+          <div class="logo me-auto">
+            <h1><a href="index2.html">Delicious Hotel</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+          </div>
+    
+          <?php 
+              
+               require_once 'menu.php';
+
+          ?>
+        </div>
+      </header><!-- End Header -->
+
+
+      
+      <main id="main">       
+      <br>
+      <br>
+<!-- ======= Contact Section ======= -->
+<section id="contact" class="contact">
+    <div class="container">
+
+      <div class="section-title">
+        <h2><span>Contact</span> Us</h2>
+        <p></p>
+      </div>
+    </div>
+
+   
+    <div class="container mt-5">
+
+      <div class="info-wrap">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 info">
+            <i class="bi bi-geo-alt"></i>
+            <h4>Location:</h4>
+            <p>A108 Adam Street<br>New York, NY 535022</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
+            <i class="bi bi-clock"></i>
+            <h4>Open Hours:</h4>
+            <p>Monday-Saturday:<br>11:00 AM - 2300 PM</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
+            <i class="bi bi-envelope"></i>
+            <h4>Email:</h4>
+            <p>info@example.com<br>contact@example.com</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
+            <i class="bi bi-phone"></i>
+            <h4>Call:</h4>
+            <p>+1 5589 55488 51<br>+1 5589 22475 14</p>
+          </div>
+        </div>
+      </div>
+
+     </div>
+  </section><!-- End Contact Section -->
+
+
+</main>
+       <footer id="footer">
+        <div class="container">
+          <h3>Delicious</h3>
+          <p>Restaurant</p>
+          
+          <div class="copyright">
+            &copy; Copyright <strong><span>Delicious</span></strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/ -->
+                      </div>
+        </div>
+      </footer><!-- End Footer -->
+    
+    <script src=" assets/vendor/bootstrap/js/bootstrap.bundle.min.js  "></script>
+  <script src=" assets/vendor/glightbox/js/glightbox.min.js  "></script>
+  <script src=" assets/vendor/isotope-layout/isotope.pkgd.min.js  "></script>
+  <script src=" assets/vendor/php-email-form/validate.js  "></script>
+  <script src=" assets/vendor/swiper/swiper-bundle.min.js  "></script>
+
+  <!-- Template Main JS File -->
+  <script src=" assets/js/main.js  "></script>
+
+</body>
+
+</html>
+
